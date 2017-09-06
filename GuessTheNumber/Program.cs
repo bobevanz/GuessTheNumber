6e6003ede3c1c36c3b04040b1daa35512fec7c9c
Bob <bobevans547@gmail.com>
2017-09-06 12:16:02 -0400

    Got the bare workings of the game complete but not able to run the game multiple times

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -6,11 +6,64 @@ using System.Threading.Tasks;
 
 namespace GuessTheNumber
 {
-    class Program
-    {  
+    class Program{
+      int GenerateMagicNumber(int HighestNumber){
+            Random rnd = new Random();
+            var MagicNumber = rnd.Next(11);
+            Debug($" The magic number is { MagicNumber}");
+            return MagicNumber;
+        }
+        int AskForTheGuess() {
+            Console.Write($"Enter your guess : ");
+            var TheGuess = Console.ReadLine();
+            int GuessNumber = int.Parse(TheGuess);
+            return GuessNumber;
+        }
+        int CompareGuessToMagicNumber(int MagicNumber, int TheGuess){
+            if (MagicNumber == TheGuess){ // is the guesss correct?            {
+                return 0;
+                   }
+            if (MagicNumber > TheGuess)
+            { //guess is too low
+                return -1;
+            }
+            if (TheGuess > MagicNumber) { //guess is too high
+                
+                    return 1;
+                
+            }
+            return -2;
+        }
+        bool PrintOutcomeResult(int result) {
+            if (result == 0){ //the guess matched - they won
+                Debug("Correct! - You Won!");
+                return true;
+            }
+            if (result== -1){ //the guess is too low
+                Debug("Too Low - Guess Again");
+                return false;
+            }
+            if (result== 1) { //the guess is too high
+                Debug("Too High - Guess Again");
+            return false;
+                    }
+            return true;          
+        }
+        void Debug(string message){
+          Console.WriteLine(message);
+        }
+        void Run(){
+            var MagicNumber = GenerateMagicNumber(100);
+            bool GameOver = false;
+            while (GameOver== false) {
+                var UserGuess = AskForTheGuess();
+                var GuessResult = CompareGuessToMagicNumber(MagicNumber, UserGuess);
+                 GameOver = PrintOutcomeResult(GuessResult);
+                }
+            }
         static void Main(string[] args)
         {
+            new Program().Run();
         }
     }
 }
- 
